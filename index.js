f5f934d3ac1523aa1e881a424abcf696b2d2442c
Harsh Bhalala <harshbhalala2002@gmail.com>
2023-02-04 21:40:27 +0530

    Minor changes in list update

--- index.js
+++ index.js
@@ -10,20 +10,19 @@ app.get("/", (req, res) => {
     const closingList = [];
     axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${req.query.symbol}&outputsize=compact&apikey=PC7NYSVUMGVGRVXH`).then(function (respose) {
         console.log("Response")
+        console.log(respose.data);
         const finalRes = respose.data["Time Series (Daily)"];
         for (key in finalRes) {
-            if(new Date(key) >= new Date(req.query.startDate.toString()) && new Date(key) <= new Date(req.query.endDate.toString())) {
-                console.log("Inside if ")
-                console.log(key)
+            if (new Date(key) >= new Date(req.query.startDate.toString()) && new Date(key) <= new Date(req.query.endDate.toString())) {
                 closingList.push(finalRes[key]['4. close']);
             }
         }
-        console.log(closingList.length)
+        
         const returnList = [];
         let baseIndex = 100;
         const indexList = [100];
-        for (let i = 1; i < closingList.length; i++) {
-            let returnVal = ((closingList[i] - closingList[i - 1]) / closingList[i - 1]) * 100;
+        for (let i = closingList.length - 2; i >= 0; i--) {
+            let returnVal = ((closingList[i] - closingList[i + 1]) / closingList[i + 1]) * 100;
             returnVal = Number(returnVal.toFixed(3))
             returnList.push(returnVal);
             baseIndex = baseIndex * (1 + (returnVal / 100));
